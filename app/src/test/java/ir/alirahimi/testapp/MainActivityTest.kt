b7a5554a2e4be9c3938ceda1903f875a66b07693
Ali Rahimi <ali.rahimi@gandom.co>
2024-08-12 12:52:14 +0330

    robo electric added

--- app/src/test/java/ir/alirahimi/testapp/MainActivityTest.kt
+++ app/src/test/java/ir/alirahimi/testapp/MainActivityTest.kt
@@ -1,9 +1,22 @@
 package ir.alirahimi.testapp
 
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class)
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+import org.junit.Assert.assertEquals
+
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [28]) // Specify the SDK version for Robolectric
 class MainActivityTest {
 
+    @Test
+    fun testActivityTitle() {
+        val activity = Robolectric.buildActivity(MainActivity::class.java).create().resume().get()
+        assertEquals("My App", activity.title)
+    }
 }
+
